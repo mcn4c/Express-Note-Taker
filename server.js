3bc541ff8fa7b22765c601de81c77cbde3648a7a
Mary C <mcn4c@virginia.edu>
2021-03-06 17:53:52 -0500

    fixed port so could deploy to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const uuid = require('uuid');
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
